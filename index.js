cb2c73ae2e99dfb3ed71b6b835651822e95b35b4
Adrian Shum <me@cshum.com>
2016-10-09 20:08:57 +0800

    raco factory function clean up

--- index.js
+++ index.js
@@ -15,7 +15,14 @@ function isObservable (val) {
 }
 function noop () {}
 
-function yieldable (raco, val, cb) {
+/**
+ * yieldable resolver
+ *
+ * @param {*} val - value to resolve
+ * @param {function} cb - callback function
+ * @returns {boolean} denote yieldable
+ */
+function yieldable (val, cb) {
   if (isPromise(val)) {
     // Promise
     val.then(function (value) {
@@ -26,7 +33,7 @@ function yieldable (raco, val, cb) {
     return true
   } else if (isGeneratorFunction(val) || isGenerator(val)) {
     // Generator
-    raco(val, cb)
+    _raco.call(this, val, [cb])
     return true
   } else if (isFunction(val)) {
     // Thunk
@@ -48,117 +55,117 @@ function yieldable (raco, val, cb) {
   }
 }
 
-module.exports = (function factory (_opts) {
+/**
+ * internal raco resolver
+ *
+ * @param {function} genFn - generator function
+ * @param {array} args - arguments in real array form
+ * @returns {promise} if no callback provided
+ */
+function _raco (genFn, args, opts) {
+  var self = this
+  var done = false
+  var trycatch = true
+  var ticking = false
+  var callback = null
+
+  opts = xtend({
+    Promise: global.Promise,
+    prepend: false,
+    yieldable: null
+  }, opts)
+
+  // pass raco next to generator function
+  if (isFunction(args[args.length - 1])) callback = args.pop()
+  // prepend or append next arg
+  if (opts.prepend) args.unshift(next)
+  else args.push(next)
+
+  var iter = isGenerator(genFn) ? genFn : genFn.apply(self, args)
+
   /**
-   * internal raco resolver
+   * internal callback stepper
    *
-   * @param {function} genFn - generator function
-   * @param {array} args - arguments in real array form
-   * @returns {promise} if no callback provided
+   * @param {object} err - callback error object
+   * @param {...*} val - callback value(s)
    */
-  function _raco (genFn, args, opts) {
-    var self = this
-    var done = false
-    var trycatch = true
-    var ticking = false
-    var callback = null
-
-    opts = xtend({
-      Promise: global.Promise,
-      prepend: false,
-      yieldable: null
-    }, _opts, opts)
-
-    // pass raco next to generator function
-    if (isFunction(args[args.length - 1])) callback = args.pop()
-    // prepend or append next arg
-    if (opts.prepend) args.unshift(next)
-    else args.push(next)
-
-    var iter = isGenerator(genFn) ? genFn : genFn.apply(self, args)
-
-    /**
-     * internal callback stepper
-     *
-     * @param {object} err - callback error object
-     * @param {...*} val - callback value(s)
-     */
-    function step (err, val) {
-      if (!iter) {
-        if (!done) {
-          done = true
-          callback.apply(self, arguments)
-        }
-      } else {
-        // generator step
-        var state
-        if (trycatch) {
-          try {
-            state = err ? iter.throw(err) : iter.next(val)
-          } catch (err) {
-            // catch err, break iteration
-            done = true
-            return callback.call(self, err)
-          }
-        } else {
-          state = err ? iter.throw(err) : iter.next(val)
-        }
-        if (state && state.done) iter = null
-        // resolve yieldable
-        var isYieldable = yieldable(raco, state.value, step)
-        if (!isYieldable && opts.yieldable) {
-          isYieldable = opts.yieldable(state.value, step)
-        }
-        // next if generator returned non-yieldable
-        if (!isYieldable && !iter) next(null, state.value)
+  function step (err, val) {
+    if (!iter) {
+      if (!done) {
+        done = true
+        callback.apply(self, arguments)
       }
-    }
-
-    /**
-     * next, callback stepper with nextTick
-     *
-     * @param {object} err - callback error object
-     * @param {...*} val - callback value(s)
-     */
-    function next () {
-      var args = Array.prototype.slice.call(arguments)
-      if (!ticking) {
-        ticking = true
-        process.nextTick(function () {
-          ticking = false
-          step.apply(self, args)
-        })
-      } else if (iter) {
-        // error on multiple callbacks wthin one iteration
-        iter = null
-        step.call(self, new Error('Multiple callbacks within one iteration'))
-      } else {
-        // callback and return, pick callback value
-        iter = null
-      }
-    }
-
-    if (callback) {
-      // callback mode
-      step()
-    } else if (opts.Promise) {
-      // return promise if callback not exists
-      return new opts.Promise(function (resolve, reject) {
-        callback = function (err, val) {
-          if (err) return reject(err)
-          resolve(val)
-        }
-        step()
-      })
     } else {
-      // callback and promise not exists,
-      // no try catch wrap
-      trycatch = false
-      callback = noop
-      step()
+      // generator step
+      var state
+      if (trycatch) {
+        try {
+          state = err ? iter.throw(err) : iter.next(val)
+        } catch (err) {
+          // catch err, break iteration
+          done = true
+          return callback.call(self, err)
+        }
+      } else {
+        state = err ? iter.throw(err) : iter.next(val)
+      }
+      if (state && state.done) iter = null
+      // resolve yieldable
+      var isYieldable = yieldable.call(self, state.value, step)
+      if (!isYieldable && opts.yieldable) {
+        isYieldable = opts.yieldable.call(self, state.value, step)
+      }
+      // next if generator returned non-yieldable
+      if (!isYieldable && !iter) next(null, state.value)
     }
   }
 
+  /**
+   * next, callback stepper with nextTick
+   *
+   * @param {object} err - callback error object
+   * @param {...*} val - callback value(s)
+   */
+  function next () {
+    var args = Array.prototype.slice.call(arguments)
+    if (!ticking) {
+      ticking = true
+      process.nextTick(function () {
+        ticking = false
+        step.apply(self, args)
+      })
+    } else if (iter) {
+      // error on multiple callbacks wthin one iteration
+      iter = null
+      step.call(self, new Error('Multiple callbacks within one iteration'))
+    } else {
+      // callback and return, pick callback value
+      iter = null
+    }
+  }
+
+  if (callback) {
+    // callback mode
+    step()
+  } else if (opts.Promise) {
+    // return promise if callback not exists
+    return new opts.Promise(function (resolve, reject) {
+      callback = function (err, val) {
+        if (err) return reject(err)
+        resolve(val)
+      }
+      step()
+    })
+  } else {
+    // callback and promise not exists,
+    // no try catch wrap
+    trycatch = false
+    callback = noop
+    step()
+  }
+}
+
+module.exports = (function factory (_opts) {
   /**
    * raco resolver
    * returns factory if no arguments
@@ -170,7 +177,7 @@ module.exports = (function factory (_opts) {
   function raco (genFn) {
     if (!isGeneratorFunction(genFn) && !isGenerator(genFn)) return factory(genFn)
     var args = Array.prototype.slice.call(arguments, 1)
-    return _raco.call(this, genFn, args)
+    return _raco.call(this, genFn, args, _opts)
   }
 
   /**
@@ -181,6 +188,7 @@ module.exports = (function factory (_opts) {
    * @returns {function} regular function
    */
   raco.wrap = function (genFn, opts) {
+    opts = xtend(_opts, opts)
     return function () {
       var args = Array.prototype.slice.call(arguments)
       return _raco.call(this, genFn, args, opts)
